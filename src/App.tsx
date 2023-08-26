dc385e86b4d1d4734f3b0764ddac39c7f8167c9c
Arvin Ramezani <arvin.ramezani6@gmail.com>
2023-08-26 18:37:21 +0330

    Add styled-components theme

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,15 @@
 import React from "react";
 import TasksSection from "./components/Tasks/TasksSection";
 import GlobalStyle from "../styles/globalStyles";
+import { ThemeProvider } from "styled-components";
+import theme from "../utils/theme";
 
 function App() {
   return (
-    <>
+    <ThemeProvider theme={theme}>
       <GlobalStyle />
       <TasksSection />
-    </>
+    </ThemeProvider>
   );
 }
 
---
+++ utils/theme.ts
@@ -0,0 +1,26 @@
+import { DefaultTheme } from "styled-components";
+
+const theme: DefaultTheme = {
+  colors: {
+    todo: {
+      background: "#FEF4F3",
+      heading: "#6E1E29",
+      cta: "#D37A87",
+      disabledText: "#D4AFB4",
+    },
+    doing: {
+      background: "#FFFBF2",
+      heading: "#795B19",
+      cta: "#C2A25B",
+      disabledText: "#DECCA4",
+    },
+    done: {
+      background: "#F4F9F3",
+      heading: "#286C1A",
+      cta: "#D37A87",
+      disabledText: "#BCD7B6",
+    },
+  },
+};
+
+export default theme;
